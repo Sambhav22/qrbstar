e45058547fb37fe6ea3b7353595113faa7e6b92d
Sambhav <jainsmbhv22@gmail.com>
2023-07-09 19:39:42 +0530

    yes

--- nes/activities/EVS/Class_01/ch_02/js/index.js
+++ nes/activities/EVS/Class_01/ch_02/js/index.js
@@ -5,7 +5,7 @@ document.getElementById("chapterName").innerText = chapter;
 
 let activityBtns = "";
 for (let i = 1; i <= noOfActivities; i++) {
-  activityBtns += `<a class="px-4 py-2 m-3 font-bold text-white bg-red-600 border rounded-md cursor-pointer" data-activity="${i}">Activity ${i}</a>`;
+  activityBtns += `<a class="px-4 py-2 m-3 font-bold text-white bg-red-600 border rounded-md cursor-pointer" data-activity="${i}">Quiz ${i}</a>`;
 }
 
 const activityBtnsContainer = document.getElementById("activityBtnsContainer");
