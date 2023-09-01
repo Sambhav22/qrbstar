2752cd87bc003cc05c30006de0792fac4962e512
Sambhav <jainsmbhv22@gmail.com>
2023-09-02 01:42:01 +0530

    changed EVS 1 Act

--- nes/activities/EVS/Class_01/ch_01/js/data.js
+++ nes/activities/EVS/Class_01/ch_01/js/data.js
@@ -3,33 +3,82 @@ export const noOfActivities = 3;
 
 if (localStorage.getItem("activityNumber") == 1) {
   activityData = {
-    activity: "Tick the correct option:",
     questions: [
       {
-        question: "We get fruits and vegetables from ",
-        optionA: "Animals",
-        optionB: "Trees",
-        optionC: "Both",
-
-        correctAnswer: "",
+        question:
+          "What benefits do trees provide to us according to the passage?",
+        optionA: "Food and shelter",
+        optionB: "Food, oxygen, and medicine",
+        optionC: "Oxygen and furniture",
+        correctAnswer: "Food, oxygen, and medicine",
       },
-
       {
-        question: "Shrubs have __________ ",
-        optionA: "Woody Stems",
-        optionB: "Both",
-        optionC: "Strong Stems",
-
-        correctAnswer: "",
+        question: "What is Ramu Kaka doing to make the school garden green?",
+        optionA: "Plucking leaves and flowers",
+        optionB: "Scratching tree trunks",
+        optionC: "Planting more trees",
+        correctAnswer: "Planting more trees",
       },
-
       {
-        question: "Big and strong plants are called ____________ ",
-        optionA: "Herbs",
+        question:
+          "What is the term used to describe big and strong plants in the passage?",
+        optionA: "Shrubs",
+        optionB: "Herbs",
+        optionC: "Trees",
+        correctAnswer: "Trees",
+      },
+      {
+        question:
+          "How are small-sized plants with woody stems referred to in the passage?",
+        optionA: "Shrubs",
         optionB: "Trees",
-        optionC: "Shrubs",
-
-        correctAnswer: "",
+        optionC: "Herbs",
+        correctAnswer: "Shrubs",
+      },
+      {
+        question:
+          "What is the term used to describe very small-sized plants with green stems?",
+        optionA: "Shrubs",
+        optionB: "Trees",
+        optionC: "Herbs",
+        correctAnswer: "Herbs",
+      },
+      {
+        question:
+          "According to the passage, what should you not do to care for the garden?",
+        optionA: "Walk on grass harshly",
+        optionB: "Pluck leaves or flowers",
+        optionC: "Scratch on tree trunks",
+        correctAnswer: "All of the above",
+      },
+      {
+        question: "What do trees provide that helps us breathe?",
+        optionA: "Food",
+        optionB: "Oxygen",
+        optionC: "Medicine",
+        correctAnswer: "Oxygen",
+      },
+      {
+        question: "What do trees give us that can be used to make furniture?",
+        optionA: "Food",
+        optionB: "Oxygen",
+        optionC: "Medicine",
+        correctAnswer: "Wood",
+      },
+      {
+        question: "What is the main idea of the passage?",
+        optionA: "The importance of school gardens",
+        optionB: "The benefits of trees to humans",
+        optionC: "Ramu Kaka's gardening skills",
+        correctAnswer: "The benefits of trees to humans",
+      },
+      {
+        question:
+          "Why should you not scratch on tree trunks, according to the passage?",
+        optionA: "It will hurt the trees",
+        optionB: "It's not mentioned in the passage",
+        optionC: "It will damage the furniture",
+        correctAnswer: "It will hurt the trees",
       },
     ],
   };
