36aab9e77c5d54d01e9d8f7e167b94a7f30ce7d2
Sambhav <jainsmbhv22@gmail.com>
2023-11-04 22:38:15 +0530

    removed modal

--- nes/activities/english/Class_03/ch_03/js/data.js
+++ nes/activities/english/Class_03/ch_03/js/data.js
@@ -5,83 +5,80 @@ if (localStorage.getItem("activityNumber") == 1) {
   activityData = {
     activity: "Tick the correct option:",
     questions: [
-      [
-        {
-          question: "What did Little George receive on his birthday?",
-          optionA: "A bicycle",
-          optionB: "An axe",
-          optionC: "A toy",
-          correctAnswer: "An axe",
-        },
-        {
-          question:
-            "How did Little George feel about his axe in the beginning?",
-          optionA: "He was excited",
-          optionB: "He was unsure if it would work",
-          optionC: "He thought it was too big",
-          correctAnswer: "He was unsure if it would work",
-        },
-        {
-          question: "What did Little George use his axe for in the courtyard?",
-          optionA: "Cutting down trees",
-          optionB: "Striking plants",
-          optionC: "Building a treehouse",
-          correctAnswer: "Striking plants",
-        },
-        {
-          question:
-            "How did Little George's father react when he saw the plants had been cut down?",
-          optionA: "He praised Little George",
-          optionB: "He shouted at the family members and servants",
-          optionC: "He laughed at the situation",
-          correctAnswer: "He shouted at the family members and servants",
-        },
-        {
-          question:
-            "Who admitted to cutting down the plants in front of George's father?",
-          optionA: "Little George",
-          optionB: "A family member",
-          optionC: "A servant",
-          correctAnswer: "Little George",
-        },
-        {
-          question: "Why did Little George cut down the plants with the axe?",
-          optionA: "He wanted to impress his father",
-          optionB: "He wanted to build a treehouse",
-          optionC: "He wanted to test if the axe works",
-          correctAnswer: "He wanted to test if the axe works",
-        },
-        {
-          question:
-            "How did Little George's father react after hearing his son's explanation?",
-          optionA: "He scolded Little George",
-          optionB: "He punished him badly",
-          optionC: "He was happy that Little George told the truth",
-          correctAnswer: "He was happy that Little George told the truth",
-        },
-        {
-          question:
-            "What advice did Little George's father give him after the incident?",
-          optionA: "Never to use an axe again",
-          optionB: "To always tell the truth",
-          optionC: "To never hurt plants because they have life too",
-          correctAnswer: "To never hurt plants because they have life too",
-        },
-        {
-          question: "What did Little George become later in life?",
-          optionA: "A famous actor",
-          optionB: "The first President of America",
-          optionC: "A famous scientist",
-          correctAnswer: "The first President of America",
-        },
-        {
-          question: "What was Little George's full name?",
-          optionA: "George Washington",
-          optionB: "George Lincoln",
-          optionC: "George Franklin",
-          correctAnswer: "George Washington",
-        },
-      ],
+      {
+        question: "What did Little George receive on his birthday?",
+        optionA: "A bicycle",
+        optionB: "An axe",
+        optionC: "A toy",
+        correctAnswer: "An axe",
+      },
+      {
+        question: "How did Little George feel about his axe in the beginning?",
+        optionA: "He was excited",
+        optionB: "He was unsure if it would work",
+        optionC: "He thought it was too big",
+        correctAnswer: "He was unsure if it would work",
+      },
+      {
+        question: "What did Little George use his axe for in the courtyard?",
+        optionA: "Cutting down trees",
+        optionB: "Striking plants",
+        optionC: "Building a treehouse",
+        correctAnswer: "Striking plants",
+      },
+      {
+        question:
+          "How did Little George's father react when he saw the plants had been cut down?",
+        optionA: "He praised Little George",
+        optionB: "He shouted at the family members and servants",
+        optionC: "He laughed at the situation",
+        correctAnswer: "He shouted at the family members and servants",
+      },
+      {
+        question:
+          "Who admitted to cutting down the plants in front of George's father?",
+        optionA: "Little George",
+        optionB: "A family member",
+        optionC: "A servant",
+        correctAnswer: "Little George",
+      },
+      {
+        question: "Why did Little George cut down the plants with the axe?",
+        optionA: "He wanted to impress his father",
+        optionB: "He wanted to build a treehouse",
+        optionC: "He wanted to test if the axe works",
+        correctAnswer: "He wanted to test if the axe works",
+      },
+      {
+        question:
+          "How did Little George's father react after hearing his son's explanation?",
+        optionA: "He scolded Little George",
+        optionB: "He punished him badly",
+        optionC: "He was happy that Little George told the truth",
+        correctAnswer: "He was happy that Little George told the truth",
+      },
+      {
+        question:
+          "What advice did Little George's father give him after the incident?",
+        optionA: "Never to use an axe again",
+        optionB: "To always tell the truth",
+        optionC: "To never hurt plants because they have life too",
+        correctAnswer: "To never hurt plants because they have life too",
+      },
+      {
+        question: "What did Little George become later in life?",
+        optionA: "A famous actor",
+        optionB: "The first President of America",
+        optionC: "A famous scientist",
+        correctAnswer: "The first President of America",
+      },
+      {
+        question: "What was Little George's full name?",
+        optionA: "George Washington",
+        optionB: "George Lincoln",
+        optionC: "George Franklin",
+        correctAnswer: "George Washington",
+      },
     ],
   };
 }
@@ -90,87 +87,85 @@ if (localStorage.getItem("activityNumber") == 2) {
   activityData = {
     activity: "Fill in the blank with correct option:",
     questions: [
-      [
-        {
-          question: "Little George received an __________ on his birthday.",
-          optionA: "axe",
-          optionB: "bicycle",
-          optionC: "book",
-          correctAnswer: "axe",
-        },
-        {
-          question:
-            "Little George was unsure if his small axe could __________ wood.",
-          optionA: "paint",
-          optionB: "cut",
-          optionC: "carve",
-          correctAnswer: "cut",
-        },
-        {
-          question:
-            "Little George decided to try out his axe by cutting down __________ in the courtyard.",
-          optionA: "plants",
-          optionB: "rocks",
-          optionC: "trees",
-          correctAnswer: "plants",
-        },
-        {
-          question:
-            "Little George's father was angry and shouting at the family members and __________.",
-          optionA: "neighbors",
-          optionB: "teachers",
-          optionC: "servants",
-          correctAnswer: "servants",
-        },
-        {
-          question:
-            "Little George's father wanted to know who had cut down the plants and threatened to __________ badly.",
-          optionA: "reward",
-          optionB: "punish",
-          optionC: "praise",
-          correctAnswer: "punish",
-        },
-        {
-          question:
-            "Little George admitted that he had cut down the plants because he wanted to see if the __________ works.",
-          optionA: "TV",
-          optionB: "bicycle",
-          optionC: "axe",
-          correctAnswer: "axe",
-        },
-        {
-          question:
-            "Little George's father was pleased that his son had told the __________.",
-          optionA: "lies",
-          optionB: "truth",
-          optionC: "stories",
-          correctAnswer: "truth",
-        },
-        {
-          question:
-            "Little George's father picked him up in his arms and told him that __________ have life too.",
-          optionA: "plants",
-          optionB: "rocks",
-          optionC: "toys",
-          correctAnswer: "plants",
-        },
-        {
-          question:
-            "In the end, Little George's full name was revealed to be __________.",
-          optionA: "John Adams",
-          optionB: "George Washington",
-          optionC: "Thomas Jefferson",
-          correctAnswer: "George Washington",
-        },
-        {
-          question:
-            "Little George became the first President of __________ later in life.",
-          optionA: "England",
-          optionB: "America",
-          optionC: "France",
-          correctAnswer: "America",
-        },
-      ],
+      {
+        question: "Little George received an __________ on his birthday.",
+        optionA: "axe",
+        optionB: "bicycle",
+        optionC: "book",
+        correctAnswer: "axe",
+      },
+      {
+        question:
+          "Little George was unsure if his small axe could __________ wood.",
+        optionA: "paint",
+        optionB: "cut",
+        optionC: "carve",
+        correctAnswer: "cut",
+      },
+      {
+        question:
+          "Little George decided to try out his axe by cutting down __________ in the courtyard.",
+        optionA: "plants",
+        optionB: "rocks",
+        optionC: "trees",
+        correctAnswer: "plants",
+      },
+      {
+        question:
+          "Little George's father was angry and shouting at the family members and __________.",
+        optionA: "neighbors",
+        optionB: "teachers",
+        optionC: "servants",
+        correctAnswer: "servants",
+      },
+      {
+        question:
+          "Little George's father wanted to know who had cut down the plants and threatened to __________ badly.",
+        optionA: "reward",
+        optionB: "punish",
+        optionC: "praise",
+        correctAnswer: "punish",
+      },
+      {
+        question:
+          "Little George admitted that he had cut down the plants because he wanted to see if the __________ works.",
+        optionA: "TV",
+        optionB: "bicycle",
+        optionC: "axe",
+        correctAnswer: "axe",
+      },
+      {
+        question:
+          "Little George's father was pleased that his son had told the __________.",
+        optionA: "lies",
+        optionB: "truth",
+        optionC: "stories",
+        correctAnswer: "truth",
+      },
+      {
+        question:
+          "Little George's father picked him up in his arms and told him that __________ have life too.",
+        optionA: "plants",
+        optionB: "rocks",
+        optionC: "toys",
+        correctAnswer: "plants",
+      },
+      {
+        question:
+          "In the end, Little George's full name was revealed to be __________.",
+        optionA: "John Adams",
+        optionB: "George Washington",
+        optionC: "Thomas Jefferson",
+        correctAnswer: "George Washington",
+      },
+      {
+        question:
+          "Little George became the first President of __________ later in life.",
+        optionA: "England",
+        optionB: "America",
+        optionC: "France",
+        correctAnswer: "America",
+      },
     ],
   };
 }
