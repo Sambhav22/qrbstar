86bf9f32a7a978094f0283c4701db76b25d4d753
Sambhav <jainsmbhv22@gmail.com>
2023-11-04 22:35:00 +0530

    removed modal

--- nes/activities/english/Class_03/ch_03/js/data.js
+++ nes/activities/english/Class_03/ch_03/js/data.js
@@ -1,80 +1,87 @@
-export const chapter = "Chapter - 2: 'a' Sound";
+export const chapter = "Chapter - 3: little George and His Axe";
 export const noOfActivities = 3;
 
 if (localStorage.getItem("activityNumber") == 1) {
   activityData = {
     activity: "Tick the correct option:",
     questions: [
-      {
-        question: "He has a can in his hand.",
-        optionA: "A bat",
-        optionB: "A ball",
-        optionC: "A can",
-        correctAnswer: "A can",
-      },
-      {
-        question: "Take this ball with the bat.",
-        optionA: "A bat",
-        optionB: "Your hands",
-        optionC: "A can",
-        correctAnswer: "A bat",
-      },
-      {
-        question: "The bag has many tags.",
-        optionA: "Books",
-        optionB: "Tags",
-        optionC: "Apples",
-        correctAnswer: "Tags",
-      },
-      {
-        question: "Apples grow in farms.",
-        optionA: "In the ocean",
-        optionB: "In farms",
-        optionC: "In the sky",
-        correctAnswer: "In farms",
-      },
-      {
-        question: "What kind of ball is it?",
-        optionA: "A small ball",
-        optionB: "A big ball",
-        optionC: "A soccer ball",
-        correctAnswer: "A big ball",
-      },
-      {
-        question: 'Who said, "This is a big ball."',
-        optionA: "Hans",
-        optionB: "Kamna",
-        optionC: "Both Hans and Kamna",
-        correctAnswer: "Hans",
-      },
-      {
-        question: "What did Kamna say about the ball?",
-        optionA: '"This is a big ball."',
-        optionB: '"This is a football."',
-        optionC: '"We shall go to the park and play."',
-        correctAnswer: '"This is a football."',
-      },
-      {
-        question: "What did Hans suggest they do with the ball?",
-        optionA: "Play at home",
-        optionB: "Go to the park and play",
-        optionC: "Keep it in the bag",
-        correctAnswer: "Go to the park and play",
-      },
-      {
-        question: "What did Kamna agree to do?",
-        optionA: "Call their friends",
-        optionB: "Play with the ball at home",
-        optionC: "Go to the store",
-        correctAnswer: "Call their friends",
-      },
-      {
-        question: "Who initiated the idea of calling their friends?",
-        optionA: "Hans",
-        optionB: "Kamna",
-        optionC: "Both Hans and Kamna",
-        correctAnswer: "Hans",
-      },
+      [
+        {
+          question: "What did Little George receive on his birthday?",
+          optionA: "A bicycle",
+          optionB: "An axe",
+          optionC: "A toy",
+          correctAnswer: "An axe",
+        },
+        {
+          question:
+            "How did Little George feel about his axe in the beginning?",
+          optionA: "He was excited",
+          optionB: "He was unsure if it would work",
+          optionC: "He thought it was too big",
+          correctAnswer: "He was unsure if it would work",
+        },
+        {
+          question: "What did Little George use his axe for in the courtyard?",
+          optionA: "Cutting down trees",
+          optionB: "Striking plants",
+          optionC: "Building a treehouse",
+          correctAnswer: "Striking plants",
+        },
+        {
+          question:
+            "How did Little George's father react when he saw the plants had been cut down?",
+          optionA: "He praised Little George",
+          optionB: "He shouted at the family members and servants",
+          optionC: "He laughed at the situation",
+          correctAnswer: "He shouted at the family members and servants",
+        },
+        {
+          question:
+            "Who admitted to cutting down the plants in front of George's father?",
+          optionA: "Little George",
+          optionB: "A family member",
+          optionC: "A servant",
+          correctAnswer: "Little George",
+        },
+        {
+          question: "Why did Little George cut down the plants with the axe?",
+          optionA: "He wanted to impress his father",
+          optionB: "He wanted to build a treehouse",
+          optionC: "He wanted to test if the axe works",
+          correctAnswer: "He wanted to test if the axe works",
+        },
+        {
+          question:
+            "How did Little George's father react after hearing his son's explanation?",
+          optionA: "He scolded Little George",
+          optionB: "He punished him badly",
+          optionC: "He was happy that Little George told the truth",
+          correctAnswer: "He was happy that Little George told the truth",
+        },
+        {
+          question:
+            "What advice did Little George's father give him after the incident?",
+          optionA: "Never to use an axe again",
+          optionB: "To always tell the truth",
+          optionC: "To never hurt plants because they have life too",
+          correctAnswer: "To never hurt plants because they have life too",
+        },
+        {
+          question: "What did Little George become later in life?",
+          optionA: "A famous actor",
+          optionB: "The first President of America",
+          optionC: "A famous scientist",
+          correctAnswer: "The first President of America",
+        },
+        {
+          question: "What was Little George's full name?",
+          optionA: "George Washington",
+          optionB: "George Lincoln",
+          optionC: "George Franklin",
+          correctAnswer: "George Washington",
+        },
+      ],
     ],
   };
 }
@@ -83,76 +90,87 @@ if (localStorage.getItem("activityNumber") == 2) {
   activityData = {
     activity: "Fill in the blank with correct option:",
     questions: [
-      {
-        question: "Apples ___ on trees.",
-        optionA: "grows",
-        optionB: "grow",
-        optionC: "growing",
-        correctAnswer: "grow",
-      },
-      {
-        question: "We need to ___ our homework before dinner.",
-        optionA: "do",
-        optionB: "does",
-        optionC: "did",
-        correctAnswer: "do",
-      },
-      {
-        question: "The cat ___ on the windowsill.",
-        optionA: "sleeping",
-        optionB: "sleeps",
-        optionC: "sleep",
-        correctAnswer: "sleeps",
-      },
-      {
-        question: "She always ___ her keys in her purse.",
-        optionA: "keeps",
-        optionB: "keep",
-        optionC: "kept",
-        correctAnswer: "keeps",
-      },
-      {
-        question: "I have a ___ for ice cream.",
-        optionA: "craving",
-        optionB: "craves",
-        optionC: "crave",
-        correctAnswer: "craving",
-      },
-      {
-        question: "The sun ___ in the morning.",
-        optionA: "rises",
-        optionB: "risen",
-        optionC: "rose",
-        correctAnswer: "rises",
-      },
-      {
-        question: "Please pass me the ___.",
-        optionA: "salt",
-        optionB: "salts",
-        optionC: "salted",
-        correctAnswer: "salt",
-      },
-      {
-        question: "My mom ___ delicious cookies.",
-        optionA: "bakes",
-        optionB: "baked",
-        optionC: "bake",
-        correctAnswer: "bakes",
-      },
-      {
-        question: "She ___ her bicycle to school every day.",
-        optionA: "rides",
-        optionB: "rode",
-        optionC: "riding",
-        correctAnswer: "rides",
-      },
-      {
-        question: "The teacher ___ the students with a smile.",
-        optionA: "greets",
-        optionB: "greeted",
-        optionC: "greeting",
-        correctAnswer: "greets",
-      },
+      [
+        {
+          question: "Little George received an __________ on his birthday.",
+          optionA: "axe",
+          optionB: "bicycle",
+          optionC: "book",
+          correctAnswer: "axe",
+        },
+        {
+          question:
+            "Little George was unsure if his small axe could __________ wood.",
+          optionA: "paint",
+          optionB: "cut",
+          optionC: "carve",
+          correctAnswer: "cut",
+        },
+        {
+          question:
+            "Little George decided to try out his axe by cutting down __________ in the courtyard.",
+          optionA: "plants",
+          optionB: "rocks",
+          optionC: "trees",
+          correctAnswer: "plants",
+        },
+        {
+          question:
+            "Little George's father was angry and shouting at the family members and __________.",
+          optionA: "neighbors",
+          optionB: "teachers",
+          optionC: "servants",
+          correctAnswer: "servants",
+        },
+        {
+          question:
+            "Little George's father wanted to know who had cut down the plants and threatened to __________ badly.",
+          optionA: "reward",
+          optionB: "punish",
+          optionC: "praise",
+          correctAnswer: "punish",
+        },
+        {
+          question:
+            "Little George admitted that he had cut down the plants because he wanted to see if the __________ works.",
+          optionA: "TV",
+          optionB: "bicycle",
+          optionC: "axe",
+          correctAnswer: "axe",
+        },
+        {
+          question:
+            "Little George's father was pleased that his son had told the __________.",
+          optionA: "lies",
+          optionB: "truth",
+          optionC: "stories",
+          correctAnswer: "truth",
+        },
+        {
+          question:
+            "Little George's father picked him up in his arms and told him that __________ have life too.",
+          optionA: "plants",
+          optionB: "rocks",
+          optionC: "toys",
+          correctAnswer: "plants",
+        },
+        {
+          question:
+            "In the end, Little George's full name was revealed to be __________.",
+          optionA: "John Adams",
+          optionB: "George Washington",
+          optionC: "Thomas Jefferson",
+          correctAnswer: "George Washington",
+        },
+        {
+          question:
+            "Little George became the first President of __________ later in life.",
+          optionA: "England",
+          optionB: "America",
+          optionC: "France",
+          correctAnswer: "America",
+        },
+      ],
     ],
   };
 }
@@ -162,64 +180,65 @@ if (localStorage.getItem("activityNumber") == 3) {
     activity: "Write 'True' for True and 'False' for False statements:",
     questions: [
       {
-        question: "This is the capital of France.",
+        question: "Is the Earth flat?",
+        optionA: "True",
+        optionB: "False",
+        correctAnswer: "False",
+      },
+      {
+        question:
+          "Did George Washington become the first President of the United States?",
         optionA: "True",
         optionB: "False",
         correctAnswer: "True",
       },
       {
-        question: "Elephants can fly.",
+        question: "Is water a solid at room temperature?",
         optionA: "True",
         optionB: "False",
         correctAnswer: "False",
       },
       {
-        question: "Water boils at 100 degrees Celsius at sea level.",
+        question: "Did Shakespeare write the play 'Romeo and Juliet'?",
         optionA: "True",
         optionB: "False",
         correctAnswer: "True",
       },
       {
-        question: "The sun rises in the west.",
+        question: "Is the capital of France London?",
         optionA: "True",
         optionB: "False",
         correctAnswer: "False",
       },
       {
-        question: "Sharks are mammals.",
+        question: "Is the sun a star?",
+        optionA: "True",
+        optionB: "False",
+        correctAnswer: "True",
+      },
+      {
+        question: "Is Antarctica the hottest continent on Earth?",
         optionA: "True",
         optionB: "False",
         correctAnswer: "False",
       },
       {
-        question: "The Earth is flat.",
+        question: "Is the moon made of green cheese?",
         optionA: "True",
         optionB: "False",
         correctAnswer: "False",
       },
       {
-        question: "Penguins can fly.",
+        question: "Is oxygen a gas that we breathe in?",
         optionA: "True",
         optionB: "False",
-        correctAnswer: "False",
+        correctAnswer: "True",
       },
       {
-        question: "Oxygen is a noble gas.",
+        question: "Is Mount Everest the tallest mountain in the world?",
         optionA: "True",
         optionB: "False",
-        correctAnswer: "False",
-      },
-      {
-        question: "The Great Wall of China is visible from space.",
-        optionA: "True",
-        optionB: "False",
-        correctAnswer: "False",
-      },
-      {
-        question: "Pluto is considered a planet in our solar system.",
-        optionA: "True",
-        optionB: "False",
-        correctAnswer: "False",
+        correctAnswer: "True",
       },
     ],
   };
